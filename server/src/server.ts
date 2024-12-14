cb25b6c1219e077423c16a22cd0a32b20f9f3a92
Francis Sanchez <francis.albert.2009@gmail.com>
2024-12-13 22:26:02 -0800

    succesfully connected to MongoDB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import app from "./app"
 import dotenv from "dotenv"
+import { connectDB } from "./lib/db"
 
 dotenv.config()
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
