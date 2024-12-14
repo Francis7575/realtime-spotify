9fbf074fe888dfb9a67676aefcfbb1a2f084f556
Francis Sanchez <francis.albert.2009@gmail.com>
2024-12-13 21:44:04 -0800

    routes creation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,19 @@
 import express from "express"
+import userRoutes from "./routes/userRoutes"
+import authRoutes from "./routes/authRoutes"
+import adminRoutes from "./routes/adminRoutes"
+import songRoutes from "./routes/songRoutes"
+import albumRoutes from "./routes/albumRoutes"
+import statRoutes from "./routes/statRoutes"
 
 const app = express()
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/admin", adminRoutes)
+app.use("/api/songs", songRoutes)
+app.use("/api/albums", albumRoutes)
+app.use("/api/stats", statRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
