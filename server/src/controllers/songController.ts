341f151dcaba6b80a035f0275c0026a890fee2c9
Francis Sanchez <francis.albert.2009@gmail.com>
2024-12-27 16:35:37 -0800

    Created songControllers and songRoutes

---
+++ server/src/controllers/songController.ts
@@ -0,0 +1,101 @@
+import { NextFunction, Request, Response } from "express";
+import { Song } from "../models/songModel";
+
+export const getAllSongs = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // -1 Descending newest to oldest
+    // 1 Ascending oldest to newest
+    const songs = await Song.find().sort({ createdAt: -1 });
+    res.status(200).json(songs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getFeaturedSongs = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // fetch 6 random songs using mongo aggregation pipeline
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 6 },
+      },
+      // display these in the UI
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1, 
+          audioUrl: 1,
+        },
+      },
+    ]);
+    res.status(200).json(songs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMadeForYouSongs = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // fetch 4 random songs using mongo aggregation pipeline
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      // display these in the UI
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        },
+      },
+    ]);
+    res.status(200).json(songs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTrendingSongs = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // fetch 6 random songs using mongo aggregation pipeline
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 6 },
+      },
+      // display these in the UI
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        },
+      },
+    ]);
+    res.status(200).json(songs);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/src/routes/songRoutes.ts
+++ server/src/routes/songRoutes.ts
@@ -1,9 +1,12 @@
+import { protectRoute, requireAdmin } from './../middleware/authMiddleware';
 import { Router } from "express";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controllers/songController";
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Song route with GET method')
-})
+router.get('/', protectRoute, requireAdmin, getAllSongs)
+router.get('/featured', getFeaturedSongs)
+router.get('/made-for-you', getMadeForYouSongs)
+router.get('/trending', getTrendingSongs)
 
 export default router
