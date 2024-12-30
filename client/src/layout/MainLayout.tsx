d74bcf9913916edb9f29d3a38ec8bb9d898dc00e
Francis Sanchez <francis.albert.2009@gmail.com>
2024-12-30 15:48:44 -0800

    Created usePlayerStore utilizing Zustand, Implemented useeffects in AudioPlayer component to handle the play and pause audio functionality and modified AlbumPage.tsx

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -6,39 +6,41 @@ import {
 } from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
 import FriendsActivity from "./components/FriendsActivity";
-// import AudioPlayer from "./components/AudioPlayer";
+import AudioPlayer from "./components/AudioPlayer";
 
 const MainLayout = () => {
   const isMobile = false;
   return (
-    <div className="h-screen bg-black text-white flex flex-col ">
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="flex flex-1 h-full overflow-hidden p-2"
-      >
-        {/* <AudioPlayer /> */}
-        {/* left sidebar */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={isMobile ? 0 : 10}
-          maxSize={30}
+    <>
+      <div className="h-screen bg-black text-white flex flex-col ">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex flex-1 h-full overflow-hidden p-2"
         >
-          <LeftSidebar />
-        </ResizablePanel>
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+          {/* left sidebar */}
+          <ResizablePanel
+            defaultSize={20}
+            minSize={isMobile ? 0 : 10}
+            maxSize={30}
+          >
+            <LeftSidebar />
+          </ResizablePanel>
+          <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-        {/* Main content */}
-        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
-          <Outlet />
-        </ResizablePanel>
+          {/* Main content */}
+          <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+            <Outlet />
+          </ResizablePanel>
 
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-        {/* right sidebar */}
-        <ResizablePanel defaultSize={20} minSize={0} maxSize={25}>
-          <FriendsActivity />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
+          <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+          {/* right sidebar */}
+          <ResizablePanel defaultSize={20} minSize={0} maxSize={25}>
+            <FriendsActivity />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+      <AudioPlayer />
+    </>
   );
 };
 
