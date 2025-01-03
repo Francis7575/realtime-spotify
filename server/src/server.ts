e8ffa139f5387da3c19973d1e5fb5dc4cd94b8c6
Francis Sanchez <francis.albert.2009@gmail.com>
2025-01-02 22:47:29 -0800

    Initialize a socket.io server and created socket.ts with different events

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import app from "./app"
 import { connectDB } from "./lib/db"
+import { createServer } from "http";
+import { initializeSocket } from "./lib/socket";    
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+initializeSocket(httpServer);
+
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 })
