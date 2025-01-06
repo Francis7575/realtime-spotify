1d5a3f78a0161a96c7aa656b4c763bd1eec1d2fa
Francis Sanchez <francis.albert.2009@gmail.com>
2025-01-06 15:38:57 -0800

    updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,22 +5,15 @@ import { initializeSocket } from "./lib/socket";
 import { Request, Response } from "express";
 
 const PORT = process.env.PORT
-
 // Added on cron-job to ping the server every 10 mins to avoid the server sleep on render for inactivity
 app.get("/test", (req: Request, res: Response) => {
-    try {
-      console.log("hello world");
-      res.status(200).send("Test route working");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Something went wrong");
-    }
-  });
+  res.status(200).send("Test route working");
+});
 
-const httpServer = createServer(app);
-initializeSocket(httpServer);
+// const httpServer = createServer(app);
+// initializeSocket(httpServer);
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 })
